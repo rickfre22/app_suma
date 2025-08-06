6ca0f2ad96b26db835c74e0bc3511b89caf645c6
rickfre22 <miguelgalvis@colegioguanenta.edu.co>
2025-08-06 08:21:25 -0500

    a

--- app_suma.py
+++ app_suma.py
@@ -1,11 +1,24 @@
 from tkinter import *
-
+from tkinter import messagebox
 # -----------------------------------------------------
 #funciones de la app
 #------------------------------------------------------
 
+def sumar():
+    c = int(a.get()) + int(b.get())
+    t_resultado.insert(INSERT,"La suma de "+ a.get() + "+" + b.get() + "casi siempre es " + str(c) + "\n")
 
-#------------------------------------------------------
+def borrar():
+    messagebox.showinfo("Suma 1.0","Los datos seran borrados...")
+    a.set("")
+    b.set("")
+    t_resultado.delete("1.0","end")
+
+def salir():
+    messagebox.showinfo("Suma 1.0","la app se cerrara...")
+    ventana_principal.destroy()
+    
+#-----------------------------------------------------
 # ventana principal
 #------------------------------------------------------
 
@@ -70,7 +83,6 @@ entry_a.place(x=487,y= 120)
 #Entry para el primer valor
 entry_b =Entry(frame_1,width=4,textvariable=b)
 entry_b.config(font=("arial", 20),justify=CENTER)
-# entry_b.focus_set()
 entry_b.place(x=682,y= 120)
 #-------------------------
 # frame 2 - panel de operaciones
@@ -80,19 +92,19 @@ frame_2.config(bg="ivory2", width=780,height=120)
 frame_2.place(x=10,y=260)
 #boton para sumar
 img_bt_sumar =PhotoImage(file="img/boton_sumar.png")
-bt_sumar  =Button(frame_2,image =img_bt_sumar,width=105,height=105)
+bt_sumar  =Button(frame_2,image =img_bt_sumar,width=105,height=105,command=sumar)
 #bt_sumar = Button(frame_2, text="sumar",width=10)
 bt_sumar.place(x=116,y=7)
 
 #boton para borrar
 img_bt_borrar =PhotoImage(file="img/boton_borrar.png")
-bt_borrar  =Button(frame_2,image =img_bt_borrar,width=105,height=105)
+bt_borrar  =Button(frame_2,image =img_bt_borrar,width=105,height=105, command=borrar)
 #bt_sumar = Button(frame_2, text="sumar",width=10)
 bt_borrar.place(x=337,y=7)
 
 #boton para salir
 img_bt_salir =PhotoImage(file="img/boton_salir.png")
-bt_salir  =Button(frame_2,image =img_bt_salir,width=105,height=105)
+bt_salir  =Button(frame_2,image =img_bt_salir,width=105,height=105,command=salir)
 #bt_sumar = Button(frame_2, text="sumar",width=10)
 bt_salir.place(x=558,y=7)
 #--------------------------
